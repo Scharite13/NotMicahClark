4ae9ccfaa7ee35203c5f7ccd567a2d035e8ef5a3
Derek Scharite <dms121691@gmail.com>
2021-01-17 14:00:12 -0500

    fix title

--- client/src/components/title/title.js
+++ client/src/components/title/title.js
@@ -6,8 +6,7 @@ class Title extends Component {
         return (
             <div className="title_container">
                 <div className="hero">
-                    <img className="title_image" src='nmc_caps.png' alt="Not Micah Clark" />
-                    <h1 className="title_text">NOT MICAH CLARK</h1>
+                    <img className="title_image" src='nmc_fulltitle.png' alt="Not Micah Clark" />    
                 </div>
             </div>
         )
