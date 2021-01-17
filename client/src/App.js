d099743b56b10abb077c09815920cc1569ac67aa
Derek Scharite <dms121691@gmail.com>
2021-01-17 06:49:04 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Title from './components/title/title';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
