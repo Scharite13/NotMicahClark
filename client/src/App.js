424cfc8e699943aa9908f92897d8876bc54ed735
Derek Scharite <dms121691@gmail.com>
2021-01-17 07:31:57 -0500

    add title comp

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Title from './components/title/title';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <Title />
     </div>
   );
 }
--- client/src/components/title/title.js
+++ client/src/components/title/title.js
@@ -4,8 +4,11 @@ import './title.css';
 class Title extends Component {
     render(){
         return (
-            <div>
-                <h1>Not Micah Clark</h1>
+            <div className="title_container">
+                <div className="hero">
+                    <img className="title_image" src='nmc_caps.png' alt="Not Micah Clark" />
+                    <h1 className="title_text">NOT MICAH CLARK</h1>
+                </div>
             </div>
         )
     }
