6210d7c6c61f71bdacfa48cfd31e7fb414a1279c
Derek Scharite <dms121691@gmail.com>
2021-01-26 06:14:03 -0500

    add state to contact form

--- client/src/components/ContactForm/contactform.js
+++ client/src/components/ContactForm/contactform.js
@@ -1,20 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.contactform.css';
 
 
 function ContactForm() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, email, message)
+        setName('') 
+        setEmail('')
+        setMessage('')       
+
+    }
 
 
     return (
         <div className="contact-container">
-
-            
-            <img className="letter-img"  src="contact_form_imagepng.png" alt="Mail Letter Image"/>
-            <form>
-                <input type="text" id="fname" name="firstname" placeholder="Name..." />
-                <input type="text" id="email" name="email" placeholder="Email..." />
-                <textarea id="subject" name="subject" placeholder="Your message..." />
-                <input type="submit" value="Submit"/>
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <input type="text" value={name} id="name" name="firstname" placeholder="Name..." onChange={(e) => setName(e.target.value)}/>
+                <input type="email" value={email} id="email" name="email" placeholder="Email..." onChange={(e) => setEmail(e.target.value)}/>
+                <img className="letter-img"  src="contact_form_imagepng.png" alt="Mail Letter"/>
+                <textarea id="subject" value={message} name="subject" placeholder="Your message..." onChange={(e) => setMessage(e.target.value)}/>
+                <input type="submit" value="Submit" />
             </form>
             
            
@@ -29,4 +40,3 @@ export default ContactForm;
 
 
 
-{/* <input type="text" id="lname" name="lastname" placeholder="Your last name..." /> */}
--- client/src/pages/HomePage/homepage.js
+++ client/src/pages/HomePage/homepage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Title from '../../components/Title/title.js';
 import Body from '../../components/Body/body.js';
+import Footer from '../../components/Footer/footer'
 
 const HomePage = () => (
     <div>
         <Title/>
         <Body/>
+        <Footer/>
     </div>
 )
 
